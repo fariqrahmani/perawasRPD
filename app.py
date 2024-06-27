327d491d947d90b759937d14d1b89cf7fe32e645
fariqrahmani <101387425+fariqrahmani@users.noreply.github.com>
2024-06-27 08:37:46 +0700

    Secret Service Account

--- app.py
+++ app.py
@@ -46,12 +46,22 @@ with tab1:
 		timestamp_now = datetime.now(pytz.timezone('Asia/Jakarta'))
 		stringTimestamp = timestamp_now.strftime("%Y-%m-%d %H:%M:%S")
 		setLog = [stringTimestamp, "Revisi KPA",kode_satker, nama_satker]
-		gc = gspread.service_account_from_dict(st.secrets["gs_service_account"])
+		
+		credentials = {
+				"type": st.secrets["type"],
+				"project_id": st.secrets["project_id"],
+				"private_key_id": st.secrets["private_key_id"],
+				"private_key": st.secrets["private_key"],
+				"client_email": st.secrets["client_email"],
+				"client_id": st.secrets["client_id"],
+				...
+		}
+
+		gc = gspread.service_account_from_dict(credentials)
 		spreadsheet = gc.open("Log User Perawas RPD").sheet1
 		spreadsheet.append_row(setLog)
 
 		# Master Data RPD
-
 		df = raw.iloc[:,[1, 2, 17, 18, 19, 20, 21, 22, 24, 26, 27, 28, 29, 31]]
 
 		df2=df.dropna()
